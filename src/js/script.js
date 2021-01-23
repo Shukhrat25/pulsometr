e65fa0d4710f179f27cad71569aade367dfbfa7b
Shukhrat <snuper25@gmail.com>
2021-01-23 19:59:14 +0600

    mask phone

--- src/js/script.js
+++ src/js/script.js
@@ -83,4 +83,5 @@ $(document).ready(function () {
 	valideForms('#consultation-form');
 	valideForms('#consultation form');
 	valideForms('#order form');
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
 });
