26e05101abaf04377a0ae5d26c6cebe096629c41
Shukhrat <snuper25@gmail.com>
2021-02-03 12:00:51 +0600

    dist

---
+++ dist/js/script.js
@@ -0,0 +1,144 @@
+const slider = tns({
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	nav: false,
+	navPosition: 'bottom',
+	responsive: {
+		320: {
+			nav: true
+		},
+		992: {
+			nav: false
+		}
+
+	}
+ });
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
+
+
+
+//tabs
+$(document).ready(function () {
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
+		});
+	};
+
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+
+	//Modal
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+	});
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
+
+	//validate
+
+	function valideForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: 'required',
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: 'Пожалуйста, введите своё имя',
+					minlength: jQuery.validator.format('Введите {0} символа!')
+				},
+				phone: 'Пожалуйста, введите свой номер телефона!',
+				email: {
+					required: 'Пожалуйста, введите свою почту',
+					email: 'Неправильно введен адрес почты'
+				}
+			}
+		});
+	};
+
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
+
+	//mask phone
+	$('input[name=phone]').mask('+7 (999) 999-99-99');
+
+	//submit email
+	$('form').submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: 'POST',
+			url: 'mailer/smart.php',
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find('input').val('');
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+	//smooth scroll and pageup
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
+	});
+
+	wow = new WOW(
+		{
+		boxClass:     'wow',      // default
+		animateClass: 'animate__animated', // default
+		offset:       0,          // default
+		mobile:       true,       // default
+		live:         true        // default
+	  }
+	  )
+	  wow.init();
+
+});
